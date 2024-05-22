4e11b0e6f1d2ce348215e18e00bd2e3c3c85ea01
Maziar Sojoudian <maziar@sojoudian.net>
2024-05-22 17:43:14 -0400

    Create the repo, add week two commands

---
+++ README.md
@@ -0,0 +1,4 @@
+## CLO 835 Summer 2024 Seneca
+
+
+
---
+++ week2/commands.sh
@@ -0,0 +1,5 @@
+ docker run -p 80:80 mcr.microsoft.com/azuredocs/aci-helloworld
+ docker run -d -p 80:80 mcr.microsoft.com/azuredocs/aci-helloworld
+ docker ps -a
+ docker stop a8a73651938d
+ docker start a8a73651938d
