1ea2f484d663911899fbfaaf1ab1e057253109d6
Maziar Sojoudian <maziar@sojoudian.net>
2024-05-22 18:49:15 -0400

    add commands relatede to the Docker

--- week2/commands.sh
+++ week2/commands.sh
@@ -1,5 +1,18 @@
- docker run -p 80:80 mcr.microsoft.com/azuredocs/aci-helloworld
- docker run -d -p 80:80 mcr.microsoft.com/azuredocs/aci-helloworld
- docker ps -a
- docker stop a8a73651938d
- docker start a8a73651938d
+docker run -p 80:80 mcr.microsoft.com/azuredocs/aci-helloworld
+docker run -d -p 80:80 mcr.microsoft.com/azuredocs/aci-helloworld
+docker ps -a
+docker stop a8a73651938d
+docker start a8a73651938d
+export K8S_COLOR=GREEN
+docker images
+docker pull nginx
+docker images
+docker rmi nginx
+docker images
+docker run -p 80:80 nginx
+#Control+C to stop Nginx
+docker run -d -p 80:80 nginx
+
+cd week2
+docker build -t pyhelloworld:0.1 .
+docker run -p 8000:8000 pyhelloworld:0.1
