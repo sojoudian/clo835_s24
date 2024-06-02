7692207ac2b538f9e13b871009bbd5583907d9b8
Maziar Sojoudian <maziar@sojoudian.net>
2024-06-02 12:54:51 -0400

    change back the mongo URI

--- week4/app.py
+++ week4/app.py
@@ -6,7 +6,7 @@ from bson import json_util
 app = Flask(__name__)
 
 # Connect to MongoDB
-mongo_client = MongoClient("mongodb://mongodb:27017")
+mongo_client = MongoClient("mongodb://localhost:27017")
 db = mongo_client['mydb']
 collection = db['items']
 
